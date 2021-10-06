23c2fe78d8c577138236f3c51201249a54cfd413
Ayush Shukla <shuklaayush247@gmail.com>
2021-10-06 16:06:18 +0530

    feat: dump image to local directory

--- fetch.py
+++ fetch.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import csv
+import hashlib
 import json
 import logging
 import requests
 import validators
 import sys
+
 from collections import defaultdict
 from pathlib import Path
+from PIL import Image, UnidentifiedImageError
+from requests.models import HTTPError
 
 # Set logging level
 logging.basicConfig(stream=sys.stdout, format="%(message)s", level=logging.INFO)
@@ -30,7 +34,27 @@ def get_csv(url):
     return csv.DictReader(lines)
 
 
-def parse_row(row):
+def get_image(url, log_ix):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        response.raw.decode_content = True
+
+        image = Image.open(response.raw).resize((256, 256))
+        return image.convert("RGB")
+    except HTTPError:
+        logging.warning(f"R{log_ix}: Unable to fetch image - {url}")
+        # logging.warning(e)
+    except UnidentifiedImageError:
+        logging.warning(f"R{log_ix}: Unable to read image - {url}")
+        # logging.warning(e)
+
+
+def write_image(image, filepath):
+    image.save(filepath)
+
+
+def parse_row(row, log_ix):
     row = {k: v.strip() for k, v in row.items()}
 
     assert len(row["name"]) > 0
@@ -40,30 +64,28 @@ def parse_row(row):
     for column in ["name", "bio"]:
         output[column] = row[column]
 
-    if validators.url(row["link"]):
+    def validate_url(column):
+        if not validators.url(row[column]):
+            if row[column]:
+                logging.warning(f"R{log_ix}: Invalid {column}")
+        return True
+
+    if validate_url("link"):
         output["link"] = row["link"].lower()
 
     for column in ["github", "linkedin", "twitter", "instagram"]:
-        if validators.url(row[column]):
+        if validate_url(column):
             output["socials"][column] = row[column].lower()
 
-    if validators.url(row["image"]):
-        slug = f"{row['name'].lower().replace(' ', '-')}-{i}"
-        # if write_image(row["image"], IMAGE_DIR / slug):
-        #     output["image"] = row["image"]
+    if validate_url("image"):
+        image = get_image(row["image"], log_ix)
+        if image is not None:
+            hash = hashlib.md5(image.tobytes()).hexdigest()
+            filepath = IMAGE_DIR / f"{hash}.jpg"
+            output["image"] = filepath.name
+            image.save(filepath)
 
     return output
-
-
-def write_image(url, filepath):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        with open(filepath, "wb") as f:
-            f.write(response.content)
-        return True
-    except:
-        return False
 
 
 def write_json(data, filepath):
@@ -73,17 +95,30 @@ def write_json(data, filepath):
 
 
 if __name__ == "__main__":
-    reader = get_csv(CSV_URL)
+    logging.info("-" * PRINT_WIDTH)
+    logging.info("FETCH".center(PRINT_WIDTH))
 
+    logging.info("-" * PRINT_WIDTH)
+    logging.info("Fetching CSV...")
+    logging.info(f"URL: {CSV_URL}")
+    reader = get_csv(CSV_URL)
+    logging.info("Done!")
+
+    logging.info("-" * PRINT_WIDTH)
+    logging.info("Parsing CSV...")
     output = []
     for i, row in enumerate(reader):
         try:
-            parsed = parse_row(row)
+            parsed = parse_row(row, log_ix=i + 2)
             output.append(parsed)
         except AssertionError:
+            logging.warning(f"R{i+2}: No name")
             continue
+    logging.info("Done!")
 
     logging.info("-" * PRINT_WIDTH)
-    logging.info("Dumping JSON APIs...")
+    logging.info("Writing JSON...")
+    logging.info(f"File: {OUTPUT_JSON}")
     IMAGE_DIR.mkdir(parents=True, exist_ok=True)
     write_json(output, OUTPUT_JSON)
+    logging.info("Done!")
